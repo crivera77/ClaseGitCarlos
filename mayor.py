37bf31f10f31807125279e293165e9902ea7d1cb
Charles Rivera <charlesrivera@Charless-MacBook-Pro.local>
2020-02-19 19:38:33 -0600

    Agregamos una validacion de edad

--- mayor.py
+++ mayor.py
@@ -1,2 +1,7 @@
-edad = int (input("Cual es tu edad"))
+edad = int (input("Cual es tu edad:"))
 print(f"TU edad es: { edad }")
+
+if edad > 18 :
+    print("Eres mayor de edad")
+else:
+    print("Sal de mi Bar! ! !")
